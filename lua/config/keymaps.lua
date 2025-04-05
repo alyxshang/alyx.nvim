33f4b31e34131d723cf0754bbef50e3e775c162d
alyxshang <179976644+alyxshang@users.noreply.github.com>
2025-04-05 19:22:22 +0200

    Hm.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -84,12 +84,75 @@ map('n', '<u>', ':undo<CR>')
 -- "visual" mode.
 map('v', '<d>', 'x')
 
+-- Setting the
+-- "move-to-start" of
+-- a word keybinding for
+-- all modes.
+map('n', '<b>', 'b')
+map('i', '<C-b>', 'b')
+map('v', '<b>', 'b')
+
+-- Setting the "move-to-end"
+-- of a word keybinding for 
+-- all modes.
+map('n', '<e>', 'e')
+map('i', '<C-e', 'e')
+map('v', '<e>', 'e')
+
+-- Setting the "move-to-start"
+-- of a line keybinding for
+-- all modes.
+map('n', '<s>', '0')
+map('i', '<C-s>', '0')
+map('v', '<s>', '0')
+
+-- Setting the "move-to-end"
+-- of a line keybinding for
+-- "normal" and "insert" mode.
+map('n', '<t>', '$')
+map('i', '<C-t>', '$')
+map('v', '<t>', '$')
+
+-- Setting the
+-- "move-to-start"
+-- of the currrent
+-- paragraph keybinding
+-- for all modes.
+map('n', '<a>', '{')
+map('i', '<C-a>', '{')
+map('v', '<a>', '{')
+
+-- Setting the
+-- "move-to-end"
+-- of the currrent
+-- paragraph keybinding
+-- for all modes.
+map('n', '<z>', '}')
+map('i', '<C-z>', '}')
+map('v', '<z>', '}')
+
+-- Setting the "move-to-start"
+-- of the current file keybinding
+-- for all modes.
+map('n', '<g>', 'gg')
+map('i', '<C-g>', 'gg')
+map('v', '<g>', 'gg')
+
+-- Setting the "move-to-end" 
+-- of the current file keybinding
+-- for all modes.
+map('n', '<m>', 'G')
+map('i', '<C-M>', 'G')
+map('v', '<m>', 'G')
+
+-- Setting the "move-one-forward"
+-- keybinding for all modes.
+map('n', '<w>', 'w')
+map('i', '<C-w>', 'w')
+map('v', '<w>', 'w')
+
+map('n', '<C-n>', ':bnext<CR>')
+map('n', '<C-p>', ':bprev<CR>')
 -- Commands
--- b -> beginning of word
--- e -> end of word
--- 0 -> start of line
--- ^ -> start of line
--- { -> down a para
--- } -> up a para
 -- gg -> top of file.
 -- G end of file.
